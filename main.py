fd948978a60be38471912e40c9383617d99dd02e
Marco E <marco.esposito@gmail.com>
2022-07-29 19:19:37 +0200

    added debug messages and wind direction sensor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import functools
-import random
 import logging
 
 from paho.mqtt import client as mqtt
@@ -9,25 +8,30 @@ import automationhat
 import json
 import configparser
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('ortomon_ws')
 
 
-def task(client: mqtt.Client, topic: str, wind_factor: float):
-    data = dict(wind=int(automationhat.analog.one.read() * wind_factor),
-                direction=random.randint(0, 5))
+def task(client: mqtt.Client, topic: str, wind_conversion_factor: float, direction_conversion_factor: float):
+    # don't allow negative values
+    wind_v = max(0.0, automationhat.analog.one.read())
+    direction_v = max(0.0, automationhat.analog.two.read())
+    log.debug('Vout: wind={}, direction={}'.format(wind_v, direction_v))
+
+    data = dict(wind=int(wind_v * wind_conversion_factor),
+                direction=int(direction_v * direction_conversion_factor))
+
     client.publish(topic, json.dumps(data))
-    log.info('Message published on topic "{}"'.format(topic))
+    log.info('Message published: topic={}, data={}'.format(topic, data))
 
 
 def on_connect(scheduler: BackgroundScheduler, config: dict, client, _userdata, _flags, rc):
     if rc == 0:
         log.info("Connected to MQTT broker")
-        fn = functools.partial(task, client,
-                               config['mqtt']['topic'],
-                               float(config['wind']['factor']))
-        scheduler.add_job(fn, 'interval', seconds=int(config['mqtt']['interval']),
-                          name='publisher')
+        fn = functools.partial(task, client, config['mqtt']['topic'],
+                               float(config['wind']['conversion_factor']),
+                               float(config['direction']['conversion_factor']))
+        scheduler.add_job(fn, 'interval', seconds=int(config['mqtt']['interval']), name='publisher')
         scheduler.start()
     else:
         log.error("Failed to connect, return code %d\n", rc)
@@ -45,7 +49,7 @@ def connect_mqtt(scheduler: BackgroundScheduler) -> mqtt.Client:
     client.tls_insecure_set(False)
 
     client.on_connect = functools.partial(on_connect, scheduler, config)
-    client.connect(config['mqtt']['broker'], int(config['mqtt']['port']))
+    client.connect(config['mqtt']['host'], int(config['mqtt']['port']))
 
     return client
 
